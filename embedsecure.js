7b0526d89e0c9b6a787e729129831a8e0e9fca88
simoeddami <simoeddami@gmail.com>
2014-07-28 03:01:44 +0000

    1

--- embedsecure.js
+++ embedsecure.js
@@ -1,3 +1,3 @@
 // JScript File
-document.write('<ifr'+'ame src="http://showsport-tv.com/update/'+ fid +'.html" width='+ v_width +' height=' + v_height + ' marginheight='0' marginwidth='0' scrolling='no' border='0' frameborder='0' allowfullscreen ></ifr'+'ame>') ;
+document.write('<ifr'+'ame src="http://showsport-tv.com/update/'+ fid +'.html" width='+ v_width +' height=' + v_height + ' marginheight="0" marginwidth="0" scrolling="no" border="0" frameborder="0" allowfullscreen ></ifr'+'ame>') ;
 /**/
