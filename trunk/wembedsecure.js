dab52575d386abbfb7b0a017819bb3db2c7c7a82
simoeddami@gmail.com <simoeddami@gmail.com@03815389-dbb0-b491-de34-c45b83b5b398>
2015-01-06 18:12:58 +0000

    no

---
+++ trunk/wembedsecure.js
@@ -0,0 +1,3 @@
+// JScript File
+document.write('<ifr'+'ame src="http://showsport-tv.com/wsp/'+ fid +'.html" width='+ v_width +' height=' + v_height + ' marginheight="0" marginwidth="0" scrolling="no" border="0" frameborder="0" allowfullscreen ></ifr'+'ame>') ;
+/**/
