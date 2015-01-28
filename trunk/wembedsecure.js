1ff10b1d1e9798761007dde2f12f983c8a6fd5dc
simoeddami@gmail.com <simoeddami@gmail.com@03815389-dbb0-b491-de34-c45b83b5b398>
2015-01-28 22:34:46 +0000

    wsp

--- trunk/wembedsecure.js
+++ trunk/wembedsecure.js
@@ -1,3 +1,3 @@
 // JScript File
-document.write('<ifr'+'ame src="http://showsport-tv.com/wsp/'+ fid +'.html" width='+ v_width +' height=' + v_height + ' marginheight="0" marginwidth="0" scrolling="no" border="0" frameborder="0" allowfullscreen ></ifr'+'ame>') ;
+document.write('<ifr'+'ame src="http://showsport-tv.com/wsp_embed.php?ch='+ fid +'" width='+ v_width +' height=' + v_height + ' marginheight="0" marginwidth="0" scrolling="no" border="0" frameborder="0" allowfullscreen ></ifr'+'ame>') ;
 /**/
